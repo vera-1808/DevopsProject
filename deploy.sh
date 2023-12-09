a16e89ee8ecb537588b0974ba4e08512521bc389
vera-1808 <145026125+vera-1808@users.noreply.github.com>
2023-12-09 16:15:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,28 +6,28 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     ./build.sh
 
     # Log in to Docker Hub (replace with your actual Docker Hub credentials)
-    docker login -u veera1808 -p dckr_pat_C7FMRRRiGBfoZvetq6DDyyYf8OU
+    docker login -u vera1808 -p dckr_pat_xxDIcXWe_KR09IrYop_gOzksgnE
 
   
     # Tag the image
-    docker tag react-appimg veera1808/dev_repo
+    docker tag react-appimg vera1808/dev_repo
 
     # Push the image to the Dev Docker Hub repository
-    docker push veera1808/dev_repo
+    docker push vera1808/dev_repo
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
     # Build your project
     ./build.sh
 
     # Log in to Docker Hub (replace with your actual Docker Hub credentials)
-    docker login -u veera1808 -p dckr_pat_C7FMRRRiGBfoZvetq6DDyyYf8OU
+    docker login -u vera1808 -p dckr_pat_xxDIcXWe_KR09IrYop_gOzksgnE
 
    
     # Tag the image
-    docker tag react-appimg veera1808/prod_repo 
+    docker tag react-appimg vera1808/prod_repo 
 
     # Push the image to the Prod Docker Hub repository
-    docker push veera1808/prod_repo
+    docker push vera1808/prod_repo
 else
     echo "Deployment error"
 fi
